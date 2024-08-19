315a8a57eda4efa47b12b09bc317f7563e12b247
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-19 22:46:22 +0600

    Amy-0.001.0

---
+++ src/Rooms.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Rooms {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println(a);
+        if (a == 18) {
+            System.out.println("A is not null");
+        }
+        else {
+            System.out.println("A is not null");
+        }
+    }
+}
