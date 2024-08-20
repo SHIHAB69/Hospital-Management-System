dec53671faa24d12db1eb20e198069b8680d5411
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-20 22:26:36 +0600

    Amy-0.001.3

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+        System.out.println("HEllo world started");
+    }
+}
